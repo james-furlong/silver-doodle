7d95bd33d1503f241473d453ef812dc83dc882fc
James Furlong <james.furlong@archa.com.au>
2019-06-27 06:55:09 +1000

    Added search result tableview cell file Added SearchResult object

---
+++ Light up the night/Light up the night/Screens/Dashboard/Views/SearchResultTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  SearchResultTableViewCell.swift
+//  Light up the night
+//
+//  Created by James Furlong on 27/6/19.
+//  Copyright © 2019 Archa. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    public func updateCell(with result: SearchResult) {
+        
+    }
+
+}
